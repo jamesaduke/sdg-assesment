21465999e1edcefd4727a030ce17bbe90183ab35
James Aduke <jamesodwuor4@gmail.com>
2020-04-14 20:06:14 +0300

    little changes

--- src/estimator.py
+++ src/estimator.py
@@ -1,5 +1,5 @@
 def estimator(data):
-    period_type = data['periodType']
+    periodtype = data['periodType']
     time_to_elapse = data['timeToElapse']
     reported_cases = data['reportedCases']
     hospital_beds = data['totalHospitalBeds']
@@ -11,7 +11,7 @@ def estimator(data):
     available_beds = 0.35 * hospital_beds
 
     # calculate the number of days
-    def normalize_days(periodtype, time_to_elapse):
+    def number_of_days(periodtype, time_to_elapse):
         if periodtype == 'days':
             days = time_to_elapse
         elif periodtype == 'weeks':
@@ -20,31 +20,40 @@ def estimator(data):
             days = time_to_elapse * 30
         return days
 
-    def infected_to_date(case):
-        days = normalize_days(period_type, time_to_elapse) // 3
+    # infections by requested time, where case = currentlyInfected
+    def infected_till_date(case):
+        days = number_of_days(periodtype, time_to_elapse) // 3
         return case * (2 ** days)
 
-    def severe_infections_by_requested_time(case):
-        return 0.15 * infected_to_date(case)
+    def severe_by_req_time(case):
+        return 0.15 * infected_till_date(case)
+
+    def hospital_bedsby_time(case):
+        return int(available_beds - severe_by_req_time(case))
+
+    def money_lost(case):
+        inf = infected_till_date(case)
+        days = number_of_days(periodtype, time_to_elapse)
+        return int((inf * income_population * income) / days)
 
     result = {
         "data": data,
         "impact": {
             "currentlyInfected": impact_cases,
-            "infectionsByRequestedTime": infected_to_date(impact_cases),
-            "severeCasesByRequestedTime": int(severe_infections_by_requested_time(impact_cases)),
-            "hospitalBedsByRequestedTime": hospital_beds_by_req_time(impact_cases),
-            "casesForICUByRequestedTime": int(0.05 * infected_to_date(impact_cases)),
-            "casesForVentilatorsByRequestedTime": int(0.02 * infected_to_date(impact_cases)),
+            "infectionsByRequestedTime": infected_till_date(impact_cases),
+            "severeCasesByRequestedTime": int(severe_by_req_time(impact_cases)),
+            "hospitalBedsByRequestedTime": hospital_bedsby_time(impact_cases),
+            "casesForICUByRequestedTime": int(0.05 * infected_till_date(impact_cases)),
+            "casesForVentilatorsByRequestedTime": int(0.02 * infected_till_date(impact_cases)),
             "dollarsInFlight": money_lost(impact_cases)
         },
         "severeImpact": {
             "currentlyInfected": severe_cases,
-            "infectionsByRequestedTime": infected_to_date(severe_cases),
-            "severeCasesByRequestedTime": int(severe_infections_by_requested_time(severe_cases)),
-            "hospitalBedsByRequestedTime": hospital_beds_by_req_time(severe_cases),
-            "casesForICUByRequestedTime": int(0.05 * infected_to_date(severe_cases)),
-            "casesForVentilatorsByRequestedTime": int(0.02 * infected_to_date(severe_cases)),
+            "infectionsByRequestedTime": infected_till_date(severe_cases),
+            "severeCasesByRequestedTime": int(severe_by_req_time(severe_cases)),
+            "hospitalBedsByRequestedTime": hospital_bedsby_time(severe_cases),
+            "casesForICUByRequestedTime": int(0.05 * infected_till_date(severe_cases)),
+            "casesForVentilatorsByRequestedTime": int(0.02 * infected_till_date(severe_cases)),
             "dollarsInFlight": money_lost(severe_cases)
         }
     }
