28c91be2ca3e66b8896e85ac1c665d858156498f
James Aduke <jamesodwuor4@gmail.com>
2020-04-14 17:58:37 +0300

    add normalize function

--- src/estimator.py
+++ src/estimator.py
@@ -1,2 +1,22 @@
 def estimator(data):
-  return data
+    period_type = data['periodType']
+    time_to_elapse = data['timeToElapse']
+    reported_cases = data['reportedCases']
+    hospital_beds = data['totalHospitalBeds']
+    income = data['region']['avgDailyIncomeInUSD']
+    income_population = data['region']['avgDailyIncomePopulation']
+
+    impact_cases = reported_cases * 10
+    severe_cases = reported_cases * 50
+    available_beds = 0.35 * hospital_beds
+
+    # calculate the number of days
+    def normalize_days(periodtype, time_to_elapse):
+        if periodtype == 'days':
+            days = time_to_elapse
+        elif periodtype == 'weeks':
+            days = time_to_elapse * 7
+        elif periodtype == 'months':
+            days = time_to_elapse * 30
+        return days
+
