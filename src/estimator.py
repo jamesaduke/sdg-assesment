10f1e1c0203d9b836f0e4658fd18f4207dab6d3d
James Aduke <jamesodwuor4@gmail.com>
2020-04-14 18:32:16 +0300

    challenge 1 trial

--- src/estimator.py
+++ src/estimator.py
@@ -27,3 +27,26 @@ def estimator(data):
     def severe_infections_by_requested_time(case):
         return 0.15 * infected_to_date(case)
 
+    result = {
+        "data": data,
+        "impact": {
+            "currentlyInfected": impact_cases,
+            "infectionsByRequestedTime": infected_to_date(impact_cases),
+            "severeCasesByRequestedTime": int(severe_infections_by_requested_time(impact_cases)),
+            "hospitalBedsByRequestedTime": hospital_beds_by_req_time(impact_cases),
+            "casesForICUByRequestedTime": int(0.05 * infected_to_date(impact_cases)),
+            "casesForVentilatorsByRequestedTime": int(0.02 * infected_to_date(impact_cases)),
+            "dollarsInFlight": money_lost(impact_cases)
+        },
+        "severeImpact": {
+            "currentlyInfected": severe_cases,
+            "infectionsByRequestedTime": infected_to_date(severe_cases),
+            "severeCasesByRequestedTime": int(severe_infections_by_requested_time(severe_cases)),
+            "hospitalBedsByRequestedTime": hospital_beds_by_req_time(severe_cases),
+            "casesForICUByRequestedTime": int(0.05 * infected_to_date(severe_cases)),
+            "casesForVentilatorsByRequestedTime": int(0.02 * infected_to_date(severe_cases)),
+            "dollarsInFlight": money_lost(severe_cases)
+        }
+    }
+
+    return result
